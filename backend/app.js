7e34b3e088c251147f91d542be935d3c1ead57d2
rafayk045 <81592991+rafayk045@users.noreply.github.com>
2023-09-13 14:09:42 +0500

    Cors packages to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-
 const mongoose = require('./database/mongoose');
 
-app.listen(3000, () => console.log("Server Connected on port 3000"));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });  
+/*^^
+CORS - Cross Origin Request Security.
+localhost:3000 - backend api
+localhost: 4200 - frontend
+*/
+
+
+
+app.use(express.json());
+app.listen(3000, () => console.log("Server Connected on port 3000 right now"));
