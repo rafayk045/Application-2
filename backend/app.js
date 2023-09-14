2a4e226ce98dd922a67044ec3a4f3faa68a234ee
rafayk045 <81592991+rafayk045@users.noreply.github.com>
2023-09-14 12:30:13 +0500

    Put the patch, delete, add list features

--- backend/app.js
+++ backend/app.js
@@ -38,14 +38,33 @@ app.get('/lists', (req, res)=> {
     .catch((error) => console.log.error());
 });
 
-app.post('/lists', (req, res)=> {
+app.post('/lists', (req, res)=> { 
     (new List({ 'title': req.body.title}))
         .save()
         .then((list) => res.send(list))
-        .catch((error) => { });
+        .catch((error) => console.log(error));
     //     const updateTitle = new List();
     //     updateTitle.title = req.body.title;
     //     updateTitle.save();
     // console.log('updateTitle',updateTitle);
 });
+
+app.get('/lists/:listId', (req,res) => {
+    List.find({ _id: req.params.listId })
+    .then((list) => res.send(list))
+    .catch((error) => console.log(error));
+});
+
+app.patch('/lists/:listId', (req,res) => {
+    List.findOneAndUpdate({ _id: req.params.listId }, { $set: req.body})
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+});
+
+app.delete('/lists/:listId', (req,res) => {
+    List.findByIdAndDelete(req.params.listId)
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+});
+
 app.listen(3000, () => console.log("Server Connected on port 3000 right now"));
